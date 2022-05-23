76d21f5e05c9cee22377e152b3f99d9c01f8d87b
Filip Owczarzak <filipowczarzak@Filips-MacBook-Pro.local>
2022-05-23 22:06:53 +0200

    Added request handler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello World');
+interface Request {
+  method: string, 
+  url: string;
+}
+
+const handleGetRecipes = (request: Request) => { console.log('List recipes ==> ', request); return; }
+const handleGetProducts = (request: Request) => { console.log('List available products ==> ', request); return; }
+const handlePostRecipe = (request: Request) => { console.log('Create recipe ==> ', request); return; }
+const unhandledRequest = (request: Request) => { console.log('Unhandled request ==> ', request); return };
+
+const requestsMap: { [requestKey: string]: Function} = {
+  'GET:/recipes': handleGetRecipes,
+  'GET:/products': handleGetProducts,
+  'POST:/recipes': handlePostRecipe
+}
+
+function handler(request: Request): void {
+  const requestKey = `${request.method}:${request.url}`;
+  return requestsMap[requestKey] ? requestsMap[requestKey](request) : unhandledRequest(request);
+}
+
+const getRecipes: Request = { method: 'GET', url: '/recipes' }
+const getProducts: Request = { method: 'GET', url: '/products' }
+const postRecipe: Request = { method: 'POST', url: '/recipes' }
+const unhandled: Request = { method: 'PUT', url: '/product' }
+
+console.log('\n------- START -------\n');
+
+handler(unhandled);
+
+console.log('\n-------- END --------');
