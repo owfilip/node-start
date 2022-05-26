b4a4449fcdd50fedd2d3af9519af6e84dca25489
owfilip <filipowczarzak@Filips-MacBook-Pro.local>
2022-05-26 20:14:06 +0200

    Added examples for if-else-if and switch-case

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,44 @@ const postRecipe: Request = { method: 'POST', url: '/recipes' }
 const unhandled: Request = { method: 'PUT', url: '/product' }
 
 console.log('\n------- START -------\n');
-
+console.log('---- Functional approach ---');
 handler(unhandled);
+
+
+console.log('\n---- If-Else-If approach ---')
+const request = getRecipes;
+
+if (request.method === 'GET' && request.url === '/recipes') {
+  console.log('List recipes ==> ', request);
+} else if (request.method === 'GET' && request.url === '/products') {
+  console.log('List available products ==> ', request);
+} else if (request.method == 'POST' && request.url === '/products') {
+  console.log('Create recipe ==> ', request);
+} else {
+  console.log('Unhandled request ==> ', request);
+}
+
+
+console.log('\n---- Switch-case approach ---');
+const requestKey = `${request.method}:${request.url}`;
+
+switch (requestKey) {
+  case 'GET:/recipes': {
+    console.log('List recipes ==> ', request);
+    break;
+  }
+  case 'GET:/products': {
+    console.log('List available products ==> ', request);
+    break;
+  }
+  case 'POST:/recipes': {
+    console.log('Create recipe ==> ', request);
+    break;
+  }
+  default: {
+    console.log('Unhandled request ==> ', request);
+  }
+}
+
 
 console.log('\n-------- END --------');
