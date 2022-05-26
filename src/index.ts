f4bb448575441221f4789c82f34cdc8e927c0344
owfilip <filipowczarzak@Filips-MacBook-Pro.local>
2022-05-26 20:58:16 +0200

    Reorganized code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+console.log('\n------- START -------\n');
+
+
+console.log('---- Functional approach ---');
 interface Request {
   method: string, 
   url: string;
 }
+const getRecipes: Request = { method: 'GET', url: '/recipes' }
+const getProducts: Request = { method: 'GET', url: '/products' }
+const postRecipe: Request = { method: 'POST', url: '/recipes' }
+const unhandled: Request = { method: 'PUT', url: '/product' }
 
 const handleGetRecipes = (request: Request) => { console.log('List recipes ==> ', request); return; }
 const handleGetProducts = (request: Request) => { console.log('List available products ==> ', request); return; }
@@ -17,15 +25,8 @@ const requestsMap: { [requestKey: string]: Function} = {
 function handler(request: Request): void {
   const requestKey = `${request.method}:${request.url}`;
   return requestsMap[requestKey] ? requestsMap[requestKey](request) : unhandledRequest(request);
-}
+};
 
-const getRecipes: Request = { method: 'GET', url: '/recipes' }
-const getProducts: Request = { method: 'GET', url: '/products' }
-const postRecipe: Request = { method: 'POST', url: '/recipes' }
-const unhandled: Request = { method: 'PUT', url: '/product' }
-
-console.log('\n------- START -------\n');
-console.log('---- Functional approach ---');
 handler(unhandled);
 
 
