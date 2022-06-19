c6d3278f0951bf3f6e2095b1dd8c5f41686c8f77
owfilip <filipowczarzak@Filips-MacBook-Pro.local>
2022-06-19 22:07:52 +0200

    Reorganizing code:

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ interface Product {
 // *** Task ***
 // Rewrite the following function to not use conditional statements
 // You may create helper functions and additional data structures at will
-// What problem the following solution has and how to mitigate it?
-function calculateInitial(product: Product): Product {
+// Try to mitigate the problem the following function has
+function calculateTax(product: Product): Product {
   if (product.size == 'small') {
     product.price *= 1.1;
     return product;
@@ -28,11 +28,8 @@ function calculateInitial(product: Product): Product {
 }
 
 // *** Solution ***
-// The function mutates it's object parameter
-function calculateSolution(product: Product): Product {
-  return taxMap[product.size] ? taxMap[product.size](product) : calculateOther(product);
-}
-
+// Rewrite using map and function references
+// The function mutates it's object parameter. Copy object using spread operator
 const calculateSmall = (product: Product) => { return { ...product, price: product.price * 1.1 } }
 const calculateMedium = (product: Product) => { return { ...product, price: product.price * 1.2 } }
 const calculateLarge = (product: Product) => { return { ...product, price: product.price * 1.3 } }
@@ -44,10 +41,16 @@ const taxMap: { [key: string]: Function } = {
   'large': calculateLarge
 }
 
+function calculateSolution(product: Product): Product {
+  return taxMap[product.size] ? taxMap[product.size](product) : calculateOther(product);
+}
+
+
+
 let p1: Product = {name: 'Coffee', price: 200, size: 'medium' };
 console.log('Initial');
 console.log(p1);
-console.log(calculateInitial(p1));
+console.log(calculateTax(p1));
 console.log(p1);
 
 let p2: Product = {name: 'Coffee', price: 200, size: 'medium' };
